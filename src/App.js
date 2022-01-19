85b8d4ec749c45e2bd183aba43df92075e80ff47
fahad alajmi <fahadalajmi126@gmail.com>
2022-01-19 13:21:08 +0300

    1st task

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-
-import './App.css';
-import Header from './Components/Header';
-import IslandForm from './Components/IslandForm';
-import IslandList from './Components/IslandList';
-
+import "./App.css";
+import Header from "./Components/Header";
+import IslandForm from "./Components/IslandForm";
+import IslandList from "./Components/IslandList";
+import React, { useState } from "react";
 
 function App() {
-
-  let island = {
+  let firstIsland = {
     name: "Boubyan",
-    img: "http://photos.wikimapia.org/p/00/02/20/48/92_big.jpg"
-  }
+    img: "http://photos.wikimapia.org/p/00/02/20/48/92_big.jpg",
+  };
+  const [island, setIsland] = useState(firstIsland);
 
   return (
-
     <div>
       <Header />
 
       <div className="homePage">
-        <IslandList />
+        <IslandList island={island} setIsland={setIsland} />
         <IslandForm island={island} />
       </div>
     </div>
   );
 }
-
 export default App;
--- src/Components/Island.js
+++ src/Components/Island.js
@@ -1,8 +1,11 @@
 import React from "react";
 
-export default function Island({ island }) {
+export default function Island({ island, setIsland }) {
+  const changeIsland = () => {
+    setIsland(island);
+  };
   return (
-    <div className="Island">
+    <div onClick={changeIsland} className="Island">
       <h3>{island.name}</h3>
       {/* YOU HAVE TO CHANGE THE VISITORS COUNT */}
       <p>Visitors 0</p>
